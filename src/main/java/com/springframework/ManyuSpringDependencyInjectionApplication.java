4affcb81d2e78e411bc7293481d99b22e31392c1
Manyu <manyu.a@gmail.com>
2020-05-13 22:25:52 +0530

    Added Maven project

---
+++ src/main/java/com/springframework/ManyuSpringDependencyInjectionApplication.java
@@ -0,0 +1,13 @@
+package com.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManyuSpringDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManyuSpringDependencyInjectionApplication.class, args);
+	}
+
+}
